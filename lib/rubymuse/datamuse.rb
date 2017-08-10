115e9e2329ca65a879c592aa54c36689a51983b4
Alex Gontar <mosegontar@gmail.com>
2017-08-09 22:27:36 -0400

    Remove require('uri')

--- lib/rubymuse/datamuse.rb
+++ lib/rubymuse/datamuse.rb
@@ -1,11 +1,12 @@
-require 'uri'
 require 'httparty'
+
 
 class Datamuse
 
   include HTTParty
 
   base_uri 'api.datamuse.com'
+
   def self.words(**kwargs)
     resp = get('/words', query: kwargs)
     resp.parsed_response
